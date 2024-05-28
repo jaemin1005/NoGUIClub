aba667df036e75866b68ca5e830cf05ac94f9590
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-29 00:24:45 +0900

    수정 : Clear 뷰 수정
    Clear뷰는 삭제하는 것이 아니라 display를 이용하여 숨김으로 표시한다

--- src/modules/ClearView.ts
+++ src/modules/ClearView.ts
@@ -1,9 +1,14 @@
+import { IsHTMLElement } from "./IsHTMLElement";
+
 export function ClearView(elem : Element, stash : Array<Element>){
   let children = elem.children;
   stash.length = 0;
 
-  while(elem.children.length > 0){
-    stash[stash.length] = children[0];
-    children[0].remove();
+  for(let elem of Array.from(children)){
+    if(IsHTMLElement(elem)){
+      elem.style.display = "none";
+      stash[stash.length] = elem;
+    }
   }
 }
+
