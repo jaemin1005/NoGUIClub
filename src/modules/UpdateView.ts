73ed885a9cf262c8152818bcee6b9b8f26fa1fd7
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-28 14:47:28 +0900

    추가 : 화면을 클리어하고 배열에 임시저장하는 함수 구현

--- src/modules/UpdateView.ts
+++ src/modules/UpdateView.ts
@@ -1,3 +1,9 @@
+//* Main DOM
+const $mainDOM = document.getElementById("main-view")!;
+//* 임시 저장할 공간.
+const stash : Array<Element> = [];
+
+
 export function UpdateView(arrStr : string[]) : void {
   const $mainView = document.getElementById("main-view")!;
 
@@ -7,3 +13,15 @@ export function UpdateView(arrStr : string[]) : void {
     $mainView.appendChild($div);
   }
 }
+
+/** 화면을 클리어하고 임시 stash에 children들을 저장한다. */
+export function CleareView(){
+  let children = $mainDOM.children;
+  stash.length = 0;
+
+  while($mainDOM.children.length > 0){
+    stash[stash.length] = children[0];
+    children[0].remove();
+  }
+}
+
