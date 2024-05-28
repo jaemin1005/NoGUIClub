5e99535eafd24e30d2bf292e6b5b724cce58e840
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-28 11:39:29 +0900

    추가 : ExecCommand 함수 틀 완성
    분기에 따라 해당 Command에 대한 함수 호출

--- src/modules/ExecCommand.ts
+++ src/modules/ExecCommand.ts
@@ -1,4 +1,29 @@
+import { findSourceMap } from "module";
+
 function ExecuteCommand(reqData : ReqData) : void {
-  const command = reqData.command;
+  const command = reqData.command.command;
+
+  switch(command){
+    case "search":
+      ExecSearchCmd(reqData);
+      break;  
+    case "create":
+      ExecCreateCmd(reqData);
+      break;
+    case "help":
+      ExecHelpCmd(reqData);
+      break;  
+  }
+}
+
+function ExecSearchCmd(reqData : ReqData){
+
+}
+
+function ExecCreateCmd(reqData : ReqData){
+
+}
+
+function ExecHelpCmd(reqData : ReqData){
 
 }
