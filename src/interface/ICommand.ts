3a5d6f47224099406ef31a9fd27848b3546be699
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-29 09:29:51 +0900

    추가 : Interface 폴더를 축하고 파일을 분류하였다.

---
+++ src/interface/ICommand.ts
@@ -0,0 +1,22 @@
+interface SearchCmd {
+  command : "search";
+  subCommand : "-f" | "-g" | null;
+}
+
+interface CreateCmd {
+  command : "create";
+  subCommand : "-m" | null;
+}
+
+interface HelpCmd{
+  command : "help";
+  subCommand : "-d" | null;
+}
+
+type Command = SearchCmd | CreateCmd | HelpCmd;
+
+interface ReqData {
+  header : "ngc";
+  command : Command;
+  value : string | null;
+}
