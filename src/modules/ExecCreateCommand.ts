55fc76525f63a62658b6cb2a5e7a5c0d5e13d00c
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-29 20:01:27 +0900

    추가 : Create Command가 실행되면 해당 기능에 대한 함수를 호출

--- src/modules/ExecCreateCommand.ts
+++ src/modules/ExecCreateCommand.ts
@@ -1,8 +1,34 @@
-type ExecCreateCmd = (reqData : ReqData) => void;
+import { ClearView } from "./ClearView";
+import { CreateKeyboardEvent } from "./CreateCmdKeyboardEvent";
+import { mapDOM } from "./GetDOM";
+import { ReadyWritePost } from "./WritePost";
+import { KeyDownEvent } from "./KeyboardEvent";
 
-let ExecCreateCmd_M : ExecCreateCmd = function(reqData){}
-let ExecCreateCmd_NULL : ExecCreateCmd = function(reqData){}
+//type ExecCreateCmd = (reqData : ReqData, event : KeyboardEvent) => void;
+//! 명령어 시나리오 1 -> 2
+//* 이벤트 연결 해제를 해야겠다. :)
+let ExecCreateCmd_NULL = function(reqData : ReqData){
 
+  const subCommand = reqData.command.subCommand;
+  const main = mapDOM.GetDOM("main-view")!
+  const input = mapDOM.GetDOM("command-text")! as HTMLInputElement; 
+  let createKeyboardEvent = null;
+
+  ReadyWritePost(main);
+
+  if(subCommand === "-m"){
+    createKeyboardEvent = new CreateKeyboardEvent(main, "body"); 
+
+  }
+  else{
+    createKeyboardEvent = new CreateKeyboardEvent(main, "head");
+  }
+
+
+  input.removeEventListener("keydown", KeyDownEvent);
+  input.addEventListener("keydown", createKeyboardEvent.EnterEvent);
+  input.addEventListener("input", createKeyboardEvent.InputEvent);
+}
 
 
 
