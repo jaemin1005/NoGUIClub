5bff9a7e792f85270eeee9ee8946438472a4de70
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-30 09:50:49 +0900

    수정 : 이벤트 제거를 위한 함수 참조를 위한 클로저 함수 인스턴스 생성
    keydownEvent의 클로저함수를 제거하기 위해 인스턴스를 생성하였다.
    이벤트의 변경사항으로 인해 Client.ts의 이벤트리스너를 변경하였다

--- src/Client.ts
+++ src/Client.ts
@@ -1,10 +1,10 @@
 import { ForceFocus } from "./modules/Focus.js";
-import { KeyDownEvent, AdjustWidth} from "./modules/KeyboardEvent.js";
+import { AdjustWidth, keydownEvent} from "./modules/KeyboardEvent.js";
 
 const $input = document.getElementById("command-text")! as HTMLInputElement;
 document.addEventListener("DOMContentLoaded", () => {ForceFocus($input)});
 //document.addEventListener("DOMContentLoaded", () => {$input.focus()});
 $input.addEventListener("input", AdjustWidth());
-$input.addEventListener("keydown", KeyDownEvent())
+$input.addEventListener("keydown", keydownEvent)
 $input.addEventListener("keydown", AdjustWidth());
 
