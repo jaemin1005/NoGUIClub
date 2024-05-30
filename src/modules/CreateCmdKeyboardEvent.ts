119aa82c7ea9e23ae1fa32291e75855c87a1df00
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-30 15:46:34 +0900

    추가: esc 키에 대한 이벤트와 필요한 프로퍼티를 추가하였다
    isEsc = 현재 esc키 클릭 여부.
    tempCurElem = 현재 content-type 임시 저장
    tempText = input.value를 임시 저장한다.

--- src/modules/CreateCmdKeyboardEvent.ts
+++ src/modules/CreateCmdKeyboardEvent.ts
@@ -1,48 +1,98 @@
 import { mapDOM } from "./GetDOM.js";
 import { WritePost } from "./WritePost.js";
 import { contentType } from "./ContentType.js";
+import { EnumCreateEscCmd } from "./EnumCreateEscCmd.js";
 
 //* Create 명령어에 대한 KeyboarEvent
 export class CreateKeyboardEvent {
-   
-  curElem : Element;
-  mainElem : Element;
 
-  constructor(mainElem : Element, start : "head" | "body"){
+  curElem: Element;
+  mainElem: Element;
+
+  //* Esc Event에 필요한 프로퍼티 :) 
+  isEsc: boolean
+  tempCurElem: Element | null;
+  tempText : string;
+
+  constructor(mainElem: Element, start: "head" | "body") {
     this.curElem = WritePost(mainElem, start);
     this.mainElem = mainElem;
+    this.isEsc = false;
+    this.tempCurElem = null;
+    this.tempText = "";
   }
 
-  EnterEvent(event : KeyboardEvent){
-    if(event.isComposing == true) return;
+  //* keydown - Enter에 대한 이벤트
+  EnterEvent(event: KeyboardEvent) {
+    if (event.isComposing == true || event.key !== "Enter") return;
 
-    if(event.key === "Enter"){   
-      const elem = mapDOM.GetDOM("command-type");
-      
-      if(elem){
-        if(elem.textContent === contentType.header){
-          elem.textContent = contentType.body;
-          this.curElem = WritePost(this.mainElem, "body");
-        }
+    const elem = mapDOM.GetDOM("command-type");
 
-        else if(elem.textContent === contentType.body){
-          elem.textContent = contentType.body;
-          this.curElem = WritePost(this.mainElem, "body");
-        }
-
-      }
-      else{
-        throw Error("잘못된 content-type");
+    if (elem) {
+      if (elem.textContent === contentType.header) {
+        elem.textContent = contentType.body;
+        this.curElem = WritePost(this.mainElem, "body");
       }
 
-      //* Enter시 초기화 :) 
-      let inputElem = event.target as HTMLInputElement
-      inputElem.value = "";
+      else if (elem.textContent === contentType.body) {
+        elem.textContent = contentType.body;
+        this.curElem = WritePost(this.mainElem, "body");
+      }
+
+      else if (elem.textContent === contentType.esc) {
+        const input = event.target as HTMLInputElement;
+        const command = input.value;
+
+        //* 나가기 명령어
+        if(command === EnumCreateEscCmd.quit){
+          
+        }
+      }
     }
+
+    else {
+      throw Error("잘못된 content-type");
+    }
+
+    //* Enter시 초기화 :) 
+    let inputElem = event.target as HTMLInputElement
+    inputElem.value = "";
   }
 
-  InputEvent(event : Event){
+  //* input - Input에 대한 이벤트
+  InputEvent(event: Event) {
+    //* Esc 키 이벤트 중 :) 
+    if (this.isEsc === true) return;
+
     let inputElem = event.target as HTMLInputElement;
     this.curElem.textContent = inputElem.value;
+  }
+
+  //* keydown - Esc키에 대한 이벤트
+  EscapeEvent(event: KeyboardEvent) {
+    if (event.isComposing === true || event.key !== "Escape") return;
+
+
+    this.isEsc = !this.isEsc
+    const elem = mapDOM.GetDOM("command-type")!;
+    const input = event.target as HTMLInputElement;
+
+    if (this.isEsc === true) {
+      elem.textContent = contentType.esc;
+      this.tempCurElem = this.curElem;
+      this.tempText = input.value;
+      input.value = "";
+    }
+
+    else {
+      this.curElem = this.tempCurElem!;
+      if (this.curElem.id === "post-header") {
+        elem.textContent = contentType.header;
+      }
+      else {
+        this.curElem.id = contentType.body;
+      }
+      input.value = this.tempText;
+    }
   }
 }
