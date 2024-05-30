c824e5e9b2a04fd1e7f35496508d7e4b29e97993
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-30 19:40:23 +0900

    추가 : mainView에 Element를 추가하는 함수
    배열을 순회하여 element를 넣는다. 만약 null이면 빈 div를 넣어준다

---
+++ src/modules/AddChildInRootElement.ts
@@ -0,0 +1,24 @@
+import { mapDOM } from "./GetDOM";
+
+/**
+ * * mainElem에 element를 넣는 클로저를 반환한다.
+ * * 클로저는 배열에 null이 있을 경우 공백역활을 하는 div를 넣는다.
+ * @param mainElem : root Element의 역활
+ * @returns 
+ */
+const Func = function(mainElem : Element){
+  const root = mainElem;
+  
+  return function(...arrElem : Array<Element | null>){
+      for(let elem of arrElem){
+        if(elem != null)
+          root.appendChild(elem);
+        else{
+          root.appendChild(document.createElement("div"));
+        }
+      }
+  }
+}
+
+const mainElem = mapDOM.GetDOM("main-view")!;
+export const AddChildInRootElement = Func(mainElem);
