2ebb360d0b0c5d99e70f3c2d5900483eba2008d2
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-31 20:20:32 +0900

    추가: EventController
    EventController는 stash에 현재 진행중인 이벤트를 담아둔다.
    Command가 변경될 경우 stash를 이용하여  Event를 제거한다.

---
+++ src/Components/EventController.ts
@@ -0,0 +1,39 @@
+import { mapDOM } from "../modules/GetDOM.js";
+
+/**
+ * * 이벤트 컨트롤러
+ */
+class EventController<T extends Element>  {
+  
+  private stash : Array<UseEventMap>
+  elem : T
+
+  constructor(controlElem : T){
+    this.elem = controlElem;
+    this.stash = []
+  }
+
+  /** stash에 있는 이벤트를 추가하여 elem의 이벤트에 등록한다. */
+  AddStash(eventMap : Array<UseEventMap>){
+    
+    this.RemoveAllListener();
+    
+    this.stash = this.stash.concat(eventMap);
+
+    for(let elemMap of this.stash){
+      this.elem.addEventListener(elemMap.type, elemMap.func as (event : Event) => void);
+    }
+  }
+
+  /** stash에 있는 이벤트를 이용하여 elem의 이벤트를 제거한다. */
+  RemoveAllListener(){    
+    for(let eventMap of this.stash){
+      this.elem.removeEventListener(eventMap.type, eventMap.func as (event : Event) => void);
+    }
+
+    this.stash = [];
+  }
+}
+
+const controlElem = mapDOM.GetDOM("command-text")! as HTMLInputElement;
+export const eventController = new EventController(controlElem);
