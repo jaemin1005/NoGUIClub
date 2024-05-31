f15d62b1e8672b13f9be663b4a8095af999aeb5d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-05-31 10:18:35 +0900

    추가 : OnDisplayView 구현
    rootElem의 자식들을 display : none을 display : block으로 수정한다

---
+++ src/modules/OnDisplayView.ts
@@ -0,0 +1,17 @@
+import { IsHTMLElement } from "./IsHTMLElement.js";
+
+/**
+ * * rootElem의 자식들 상태 변경
+ * * Display : none => Display : block
+ * @param rootElem 
+ */
+export function OnDisplayView(rootElem : Element){
+  const children = rootElem.children;
+
+  for(let elem of Array.from(children)){
+    //* HTMLElement인지 확인, display 상태가 none인지
+    if(IsHTMLElement(elem) && elem.style.display === "none"){
+      elem.style.display = "block";
+    }
+  }
+}
