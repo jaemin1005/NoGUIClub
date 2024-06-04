a811dd415cd1ef124e9f7279386ddc47918bb538
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-04 21:05:12 +0900

    add: node express 환경 조성

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,2 +1,11 @@
-import * as express from "express"
-const app = express.default();
+import express from "express"
+import { LoadFile } from "./modules/LoadFile";
+
+const app = express();
+
+app.get("/", (req, res) => {LoadFile("index.html", res)});
+
+app.listen(3000, () => {
+  console.log("서버 시작 되었습니다.");
+  console.log("http://localhost:3000");
+})
