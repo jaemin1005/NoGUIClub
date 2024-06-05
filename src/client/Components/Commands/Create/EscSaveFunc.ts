dee029a23cf0eedd0ed94f5cf90d3a532e66d23a
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-05 19:27:09 +0900

    new: --save 명령어 부분 완성
    callback 함수 구현 나중에 구현할 예정

--- src/client/Components/Commands/Create/EscSaveFunc.ts
+++ src/client/Components/Commands/Create/EscSaveFunc.ts
@@ -3,6 +3,7 @@ import { OnDisplayView } from "../../../modules/OnDisplayView";
 import { eventController } from "../../EventController";
 import { NormalEvent } from "../../../modules/ArrNormalEvent";
 import { ConvertPostIntoData } from "../../../modules/ConvertPostIntoData";
+import { POSTFetch } from "client/modules/POSTfetch";
 
 //* 1. 현재 작성한글을 서버로 전송한다.
 //* 2. 명령어를 되돌린다.
@@ -11,13 +12,15 @@ import { ConvertPostIntoData } from "../../../modules/ConvertPostIntoData";
 export function EscSaveFunc(rootElem : Element){
 
     //* 작성한 글을 데이터로 만들기.
-    ConvertPostIntoData(rootElem);
-    //* 서버로 전송하기.
-
+    const data = ConvertPostIntoData(rootElem);
+    
     //* 이벤트 되돌리기.
     eventController.AddStash(NormalEvent());
 
     //* View화면 되돌리기.
     DeleteView(rootElem);
     OnDisplayView(rootElem);
+
+    //* 서버로 전송하기.
+    POSTFetch("/create", JSON.stringify(data));
 }
