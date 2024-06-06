2bec5c8d48e83bfc304f92034f487d068404f827
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-06 23:31:32 +0900

    new: 서버에 body-parser 미들웨어를 추가하였음

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,12 +1,17 @@
 import express from "express"
+import bodyParser from "body-parser";
 import { LoadFile } from "./modules/LoadFile";
 import { SavePostFunc } from "./controllers/SavePostFunc";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 //* 정적 파일 전송
 app.use("/public", express.static("public"));
 app.use("/dist", express.static("dist"));
+
 //* 타입스크립트 전송 (디버깅용입니다)
 app.use("/src", express.static("src"));
 
