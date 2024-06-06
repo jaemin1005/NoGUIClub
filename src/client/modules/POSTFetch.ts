11dfe7975bec83a52ec7195f281ffdfad00a4805
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-06 23:27:20 +0900

    hotfix: header에 Content-type 추가

--- src/client/modules/POSTFetch.ts
+++ src/client/modules/POSTFetch.ts
@@ -1,7 +1,12 @@
 export async function POSTFetch(url : string, data : string, callback? : IResponseCbFunc, errCallback? : IResponseCbFunc){
   const host = window.location.origin + url; 
 
-  let res = await fetch(host,{ method : "POST", body : data });
+  let res = await fetch(host,{ 
+    method : "POST",
+    headers : {
+      'Content-Type': 'application/json'
+    },
+    body : data });
   if(res.status === 200){
     if(callback) callback(res);
   }else{
