697652097f48e39621c3b0a4667e3cd12ca43bc4
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-07 21:27:27 +0900

    modify: 데이터베이스에 해당 데이터를 저장하는 로직을 추가하였다

--- src/server/controllers/SavePostFunc.ts
+++ src/server/controllers/SavePostFunc.ts
@@ -2,7 +2,8 @@ import { IRouterCbFunc } from "../interfaces/IRouterCbfunc";
 import { WriteFile } from "../modules/WriteFile"
 import { TransObjIntoHash } from "../modules/TransObjIntoHash";
 import { SimpleNAck } from "../modules/SimpleNAck";
-
+import { db } from "server/modules/NGCDatabase";
+import { IData } from "@shared/interface/IData";
 
 /**
  * * 글을 저장하는 컨트롤러.
@@ -14,11 +15,16 @@ import { SimpleNAck } from "../modules/SimpleNAck";
 export const SavePostFunc: IRouterCbFunc = (req, res) => {
 
   if (req.body) {
+
+    const data = req.body as IData;
+
     const path = "resources/post/"
     const extenstion = ".json"
-    const data = JSON.stringify(req.body);
-    const fileName = TransObjIntoHash(data);
-    WriteFile(path + fileName + extenstion, data, res);
+    const jsonData = JSON.stringify(req.body);
+    const fileName = TransObjIntoHash(data) + extenstion;
+    WriteFile(path + fileName, jsonData, res);
+    db.SavePost(data, fileName);
+
   } else {
     SimpleNAck(res);
   }
