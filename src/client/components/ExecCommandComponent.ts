d24a2eb8e95429f0bd75a0068142c6f06e7da2f8
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-07 13:21:36 +0900

    hofix: 폴더 경로 수정
    폴더 대소문자 구분

---
+++ src/client/components/ExecCommandComponent.ts
@@ -0,0 +1,26 @@
+import { CheckCommand, CorrectCommand } from "../modules/CheckCommand";
+import { ExecuteCommand } from "./commands/ExecCommand";
+import { AddErrorInView } from "./commands/ErrorCommand";
+
+//* Command 실행 절차.
+//* (1) Header가 있는지, Command 체크 => CheckCommand
+//* (2) SubCommand가 있는지 => CorrectCommand
+//* (3) Command 실행 => ExecuteCommand
+export function ExecCmd(value : string){
+  const isCommand = CheckCommand(value);
+  if(isCommand){
+    if(CorrectCommand(isCommand)){
+      ExecuteCommand(isCommand);
+    }
+
+    else{
+      //TODO 에러처리를 해야 된다.
+      AddErrorInView("옳바른 값이 아닙니다", [`${value}을 다시 확인해 보세요`]);
+    }
+  }
+
+  else{
+    //TODO 에러처리를 해야 된다. 
+    AddErrorInView("주요 커맨드가 아닙니다.", [`${value}은 옳바른 커맨드가 아닙니다.`]);
+  }
+}
---
+++ src/client/components/commands/create/WritePost.ts
@@ -0,0 +1,27 @@
+import { enumPostElemName } from "../../../modules/Enum/EnumPostElemName";
+
+/**
+ * * 글 쓰기 작업을 한다.
+ * * type에 따라 header, body에 글을 쓴다.
+ * @param elem 
+ * @param type 
+ * @returns 
+ */
+export function WritePost(elem : Element, type : PostType) : Element{
+  let curElem : null | Element = null;
+
+  if(type === "head"){
+    curElem = document.getElementById(enumPostElemName.header)!;
+  }
+
+  else{
+    curElem = document.createElement("div");
+    curElem.className = enumPostElemName.body;
+    elem.appendChild(curElem);
+  }
+
+  return curElem;
+}
+
+
+
