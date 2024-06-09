a3e28a205287a32b0dc942cf3da7033f4cbb0f29
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-09 15:55:01 +0900

    new: search 명령어 구현

--- src/client/components/commands/search/ExecSearchCommand.ts
+++ src/client/components/commands/search/ExecSearchCommand.ts
@@ -1,3 +1,22 @@
+import { POSTFetch } from "../../../modules/POSTFetch"
+import { SearchKeyboardEvent } from "./SearchKeyboardEvent"
+import { mapDOM } from "../../../modules/GetDOM"
+import { IData } from "@shared/interface/IData";
+import { SearchListView } from "./SearchListView";
+
 export function ExecSearchCmd(reqData : ReqData, command : SearchCmd){
-  
+  POSTFetch("/search", JSON.stringify(reqData), SearchSuccessCbFunc)
 }
+
+async function SearchSuccessCbFunc(res : Response){
+  const elem = mapDOM.GetDOM("command-text")!;
+  const data : IData[] = await res.json();  
+  const KeyboardEvent = new SearchKeyboardEvent(elem);
+  KeyboardEvent.SetMapPageData(1, data);
+  SearchListView(KeyboardEvent.GetCurrentPage()!);
+}
+
+
+
+
+
