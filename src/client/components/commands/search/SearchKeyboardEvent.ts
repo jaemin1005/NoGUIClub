6ce4ffd821883dfba943b4b959de60b0fdb7851b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-09 21:31:21 +0900

    modify: IData의 구현체의 변경으로 인한 Enter이벤트를 수정하였음

--- src/client/components/commands/search/SearchKeyboardEvent.ts
+++ src/client/components/commands/search/SearchKeyboardEvent.ts
@@ -1,30 +1,32 @@
 import { CommandKeyboardEvent } from "../../../modules/CommandKeyboardEvent";
 import { AddChildInRootElement } from "../../../modules/AddChildInRootElement";
-import { WritingData } from "../../../../shared/modules/WritingData";
+import { Data } from "../../../../shared/modules/Data";
 import { IData } from "@shared/interface/IData";
+import { CreateElement } from "client/modules/CreateElement";
+import { postHeadStyle } from "client/styles/PostHeadElem";
 
 export class SearchKeyboardEvent extends CommandKeyboardEvent{
 
   nCurPage : number; 
-  mapPageData : Map<number, Array<WritingData>>
+  mapPageData : Map<number, Array<Data>>
   
   constructor(elem: Element){
     super(elem);
     this.nCurPage = 1;
-    this.mapPageData = new Map<number, Array<WritingData>>();
+    this.mapPageData = new Map<number, Array<Data>>();
   }
 
   SetMapPageData(page: number, arrData: IData[]){
-    const arrWritingData : Array<WritingData> = [];
+    const arrWritingData : Array<Data> = [];
 
     for(const data of arrData){
-      arrWritingData[arrWritingData.length] = new WritingData(data.head, data.date, data.body);    
+      arrWritingData[arrWritingData.length] = new Data(data.head, data.date, data.body);    
     }
 
     this.mapPageData.set(page, arrWritingData);
   }
 
-  GetCurrentPage() : Array<WritingData> | undefined {
+  GetCurrentPage() : Array<Data> | undefined {
     return this.mapPageData.get(this.nCurPage);
   }
 
@@ -41,7 +43,14 @@ export class SearchKeyboardEvent extends CommandKeyboardEvent{
 
     const data = arrData[num];
 
-    AddChildInRootElement(null, null, data.TransHeadIntoElem(), null, null, ...data.TransBodyIntoElem());
+    const head = CreateElement({elem : "div", property : {textContent : data.head}, style : postHeadStyle })
+    const body : Array<Element> = []
+    
+    data.body.forEach(textcontent => {
+      const elem = CreateElement({elem : "div", property : {textContent : textcontent}})
+      body[body.length] = elem;
+    })
+    AddChildInRootElement(null, null, head, null, null, ...body);
   }
 
   EscapeCbFunc(event: KeyboardEvent): void {
