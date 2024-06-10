e374c70bf6faff6d751a4a48533c2a93ed9ab455
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-11 00:39:09 +0900

    modify: search명령어 응답이 성공 했을 떄 콜백함수 수정
    search이벤트에 맞는 이벤트를 할당해주고, 글 목록 페이지를 보여준다

--- src/client/components/commands/search/ExecSearchCommand.ts
+++ src/client/components/commands/search/ExecSearchCommand.ts
@@ -4,6 +4,8 @@ import { mapDOM } from "../../../modules/GetDOM"
 import { IData } from "@shared/interface/IData";
 import { SearchListView } from "./SearchListView";
 import { ICommandData, SearchCmd } from "@shared/interface/ICommand";
+import { eventController } from "client/components/EventController";
+import { ArrSearchEvent } from "./ArrSearchEvent";
 
 export function ExecSearchCmd(reqData : ICommandData, command : SearchCmd){
   POSTFetch("/search", JSON.stringify(reqData), SearchSuccessCbFunc)
@@ -12,9 +14,10 @@ export function ExecSearchCmd(reqData : ICommandData, command : SearchCmd){
 async function SearchSuccessCbFunc(res : Response){
   const elem = mapDOM.GetDOM("command-text")!;
   const data : IData[] = await res.json();  
-  const KeyboardEvent = new SearchKeyboardEvent(elem);
-  KeyboardEvent.SetMapPageData(1, data);
-  SearchListView(KeyboardEvent.GetCurrentPage()!);
+  const keyboardEvent = new SearchKeyboardEvent(elem);
+  keyboardEvent.SetMapPageData(1, data);
+  eventController.AddStash(ArrSearchEvent(keyboardEvent));
+  SearchListView(keyboardEvent.GetCurrentPage()!);
 }
 
 
