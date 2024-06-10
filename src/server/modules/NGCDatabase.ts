ba8ff1a8bf668a2ec70532dd6fdbda7c8f21fd54
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-10 11:33:37 +0900

    new: 데이터베이스 글을 조회하는 명령어 구현

--- src/server/modules/NGCDatabase.ts
+++ src/server/modules/NGCDatabase.ts
@@ -1,16 +1,26 @@
 import { Data } from "@shared/modules/Data";
 import { DataBase } from "./Database";
 import { TransArrStrIntoStr } from "@shared/modules/TransArrStrIntoStr"
+import { GetKeywords } from "./GetKeyword";
 
 class NGCDatabase extends DataBase{
 
   SavePost(data : Data, fileName : string){
-    const query = "INSERT INTO post_table (head, file_name, date) VALUES(?,?,?)"
-    const params = [data.head, fileName, data.date];
+    const query = "INSERT INTO post_table (head, file_name, date, keywords) VALUES(?,?,?,?)"
+    const keywords = GetKeywords(data.ReturnArrStr());
+    const params = [data.head, fileName, data.date, TransArrStrIntoStr(keywords)];
     
-    const keywords = TransArrStrIntoStr(data.ReturnArrStr());
-
     this.ExecQuery(query, params);
+  }
+
+  async FindPost(data : Data, page : number){
+
+    const nMinNum = (page - 1) * 10;
+    const nMaxNum = page * 10 - 1
+
+    const query = `SELECT * FROM post_table ORDER BY create_at DESC LIMIT ${nMinNum}, ${nMaxNum}`;
+    const result = await this.ExecQuery(query);
+    return result;
   }
 }
 
