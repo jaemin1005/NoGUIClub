db2a73df43e017280bbe7d2883f13341240bec23
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-11 00:37:12 +0900

    modify: 매개변수를 SearchKeyboardEvent타입을 받도록 설정
    해당 매개변수를 이용하여 이벤트 배열을 만든다.

--- src/client/components/commands/search/ArrSearchEvent.ts
+++ src/client/components/commands/search/ArrSearchEvent.ts
@@ -3,17 +3,16 @@ import { SearchKeyboardEvent } from "./SearchKeyboardEvent";
 import { mapDOM } from "client/modules/GetDOM";
 
 
-export function ArrSearchEvent(mainElem : Element, start : "head" | "body"){
-  const execCreateEvent = new SearchKeyboardEvent(mapDOM.GetDOM("command-text")!);
+export function ArrSearchEvent(searchKeyboardEvent : SearchKeyboardEvent){
 
   const enter : IEventMap<"keydown", HTMLInputElement> = {
     type : "keydown",
-    func : (event) => execCreateEvent.Enter(event)
+    func : (event) => searchKeyboardEvent.Enter(event)
   };
   
   const escape : IEventMap<"keydown", HTMLInputElement> = {
     type : "keydown",
-    func : (event) => execCreateEvent.Escape(event)
+    func : (event) => searchKeyboardEvent.Escape(event)
   }
 
   const adjust : IEventMap<"keydown", HTMLInputElement> = {
