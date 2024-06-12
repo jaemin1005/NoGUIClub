78cefa1dae8f64f520f55767c2fa4a261d5fedb0
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-12 16:24:11 +0900

    new : Theme
    테마에 대한 클래스 모듈
    프로퍼티 __theme에 따라 스타일이 변경되도록 적용할거임

---
+++ src/client/modules/Theme.ts
@@ -0,0 +1,17 @@
+class Theme {
+  __theme
+
+  constructor(theme : TypeTheme){
+    this.__theme = theme   
+  }
+
+  set Theme(value : TypeTheme){
+    this.__theme = value;
+  }
+
+  get Theme(){
+    return this.__theme;
+  }
+}
+
+export const theme = new Theme("origin");
