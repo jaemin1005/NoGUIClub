11420d207c08756f4bda54a17f5273785dc52887
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-12 16:17:20 +0900

    modify | PostHeadElem
    글 제목에대한 Style에 대한 Map 지정

--- src/client/styles/PostHeadElem.ts
+++ src/client/styles/PostHeadElem.ts
@@ -1,7 +1,12 @@
 import { HexaIntoRGB } from "client/modules/HexaIntoRGB";
 
-export const postHeadStyle : Partial<CSSStyleDeclaration> = {
+const postHeadStyle : Partial<CSSStyleDeclaration> = {
   color : HexaIntoRGB("#F4BF4F"),
   fontSize : "1.5rem",
   height : "1.5rem",
 }
+
+const postHeadStyleMap : StyleMap = new Map();
+postHeadStyleMap.set("origin", postHeadStyle);
+
+export { postHeadStyleMap };
