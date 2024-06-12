12f311c0ac61180185224903d857ca984adc094c
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-12 16:01:23 +0900

    modify | SearchListStyle
    map으로 반환하여 여러 테마에 적용할 수 있게 하였다.

--- src/client/styles/SearchListStyle.ts
+++ src/client/styles/SearchListStyle.ts
@@ -1,5 +1,11 @@
-export const searchListStyle : Partial<CSSStyleDeclaration> = {
+
+const searchListStyle : Partial<CSSStyleDeclaration> = {
   "fontSize" : "1rem",
   "marginTop" : "0.5rem",
   "marginBottom" : "0.5rem"
 }
+
+const searchListStyleMap : StyleMap = new Map<Theme, Partial<CSSStyleDeclaration>>();
+searchListStyleMap.set("origin", searchListStyle);
+
+export {searchListStyleMap};
