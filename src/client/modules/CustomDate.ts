b0a308837f9d98cf09b6f35bd2d90b9e783aad3b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-13 12:07:23 +0900

    modify | CustomData
    UnixIntoTime Method 수정
    유닉스 시간을 시분초로 정확히 나타나지 않았음
    매개변수에 * 1000을 하여 시점을 정확히 나타날 수 있게함

--- src/client/modules/CustomDate.ts
+++ src/client/modules/CustomDate.ts
@@ -19,15 +19,15 @@ class CustomDate {
   }
 
   UnixIntoTime(unixTime : number) : string {
-    const date = new Date(unixTime);
+    const date = new Date(unixTime * 1000);
 
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2,'0');
     const day = date.getDate().toString().padStart(2,'0');
 
-    const hour = date.getHours();
-    const minutes = date.getMinutes();
-    const second = date.getSeconds();
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2,'0');
 
 
     return `${year}-${month}-${day} ${hour}:${minutes}:${second}`;
