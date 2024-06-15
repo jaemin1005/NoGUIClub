f495421af73288f1597ca36327382a537ce86111
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-15 21:15:39 +0900

    new | GetServerState
    브라우저의 환경 OS 정보를 받아오고, 서버에게 POST 요청하는 모듈
    응답으로, 서버의 상태를 받아온다

---
+++ src/client/controllers/GetServerState.ts
@@ -0,0 +1,24 @@
+import { browserName } from "client/modules/GetUsingBrowswer"
+import { osName } from "client/modules/GetUsingOS"
+import { POSTFetch } from "client/modules/POSTFetch"
+
+//* 초기 서버 상태, Init
+const serverState : IServerState = {
+  MySQLConnect : false,
+}
+
+const userInfo = {
+  browserName : browserName,
+  osName : osName
+}
+
+await POSTFetch("/userinfo", JSON.stringify(userInfo), async (res)  => {
+  const data = await res.json() as IServerState
+  let key : keyof typeof data
+
+  for(key in data){
+    serverState[key] = data[key];
+  }
+})
+
+export {serverState};
