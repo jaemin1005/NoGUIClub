7c05e822c0273fa56232079c35d4a140b27d683b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-17 02:18:44 +0900

    Modify | CreateCmdIndexedDB
    EscSaveFunc 모듈 분리에 의한, Export 경로 변경

--- src/client/controllers/IndexedDBController/CreateCmdIndexedDB.ts
+++ src/client/controllers/IndexedDBController/CreateCmdIndexedDB.ts
@@ -1,9 +1,15 @@
 import { indexedDB } from "./CallIndexedDB";
 import { DbTableName } from "client/modules/Enum/EnumDbTableName";
 import { IData } from "@shared/interface/IData";
+import { SaveFailFunc, SaveSuccessFunc } from "../commands/create/EscSaveCbFunc";
+import { mapDOM } from "client/modules/GetDOM";
 
-export const CreateCmdIndexedDB = async (obj : IData, cbFunc : Function, errCbFunc : Function ) => {
+export const CreateCmdIndexedDB = async (obj : IData, cbFunc? : Function, errCbFunc? : Function ) => {
   const result = await indexedDB.Add(DbTableName.POST, obj);
-  if(result) cbFunc();
-  else errCbFunc();
+  if(result){
+    SaveSuccessFunc(mapDOM.GetDOM("main-view")!);
+  }
+  else{
+    SaveFailFunc(mapDOM.GetDOM("main-view")!);
+  }
 }
