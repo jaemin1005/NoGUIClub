6cb997bb7220d8fc5bd64872edae78160d91d824
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-16 16:05:22 +0900

    Modify | TypeSendData
    ProcessCmdIndexedDB 매개변수 콜백함수 타입 변경

--- src/client/interface/TypeSendData.ts
+++ src/client/interface/TypeSendData.ts
@@ -1,2 +1,2 @@
 type SendData = (url : string, obj : {}, cbFunc : IResponseCbFunc, errCbFunc : IResponseCbFunc) => void;
-type ProcessCmdIndexedDB = (obj : {}, cbFunc : IResponseCbFunc, errCbFunc : IResponseCbFunc ) => void;
+type ProcessCmdIndexedDB = (obj : {}, cbFunc : Function, errCbFunc : Function ) => void;
