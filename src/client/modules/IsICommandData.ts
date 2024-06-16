3d7e9cfbbce1cc431b9d8de4e265910a2ddc0bc1
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-16 21:35:15 +0900

    New | IsICommandData
    ProcessData를 ICommandData로 분리하기 위한 타입 가드

---
+++ src/client/modules/IsICommandData.ts
@@ -0,0 +1,7 @@
+import { ProcessData } from "client/interface/TypeSendData";
+import { ICommandData } from "@shared/interface/ICommand";
+
+export function IsICommandData(obj : ProcessData) : obj is ICommandData {
+  return ( obj as ICommandData ).command !== undefined && (obj as ICommandData).header !== undefined &&
+    (obj as ICommandData).value !== undefined
+}
