5d0e234852a9926c9f7e6ab2e1960c5ad4928c93
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-16 21:36:35 +0900

    Modify | CreateCmdIndexedDB
    타입으로 지정안하고 매개변수의 타입을 변경

--- src/client/controllers/IndexedDBController/CreateCmdIndexedDB.ts
+++ src/client/controllers/IndexedDBController/CreateCmdIndexedDB.ts
@@ -1,9 +1,8 @@
 import { indexedDB } from "./CallIndexedDB";
 import { DbTableName } from "client/modules/Enum/EnumDbTableName";
+import { IData } from "@shared/interface/IData";
 
-
-
-export const CreateCmdIndexedDB : ProcessCmdIndexedDB = async (obj, cbFunc, errCbFunc ) => {
+export const CreateCmdIndexedDB = async (obj : IData, cbFunc : Function, errCbFunc : Function ) => {
   const result = await indexedDB.Add(DbTableName.POST, obj);
   if(result) cbFunc();
   else errCbFunc();
