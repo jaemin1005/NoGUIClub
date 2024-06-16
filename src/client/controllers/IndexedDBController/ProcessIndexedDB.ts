e8a9fb5b6ebe450c87911abcb14532cde73b9250
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-17 02:44:08 +0900

    Modify | ProcessIndexedDB
    SearchCmdIndexedDB가 Promise를 반환하므로 async를 추가
    Search 결과에 따른 콜백메소드 구현

--- src/client/controllers/IndexedDBController/ProcessIndexedDB.ts
+++ src/client/controllers/IndexedDBController/ProcessIndexedDB.ts
@@ -3,8 +3,13 @@ import { SearchCmdIndexedDB } from "./SearchCmdIndexedDB";
 import { TypeSendData } from "client/interface/TypeSendData";
 import { IsIData } from "client/modules/IsIData";
 import { IsICommandData } from "client/modules/IsICommandData";
+import { SearchSuccessCbFunc } from "../commands/search/ExecSearchCbFunc";
 
-export const ProcessIndexedDB : TypeSendData = (url, obj, cbFunc, errCbFunc) => {
+export const ProcessIndexedDB : TypeSendData = async (url, obj, cbFunc, errCbFunc) => {
   if(url === "/create" && IsIData(obj)) CreateCmdIndexedDB(obj, cbFunc, errCbFunc);
-  else if(url === "/search" && IsICommandData(obj)) SearchCmdIndexedDB(obj);
+
+  else if(url === "/search" && IsICommandData(obj)) {
+    const data = await SearchCmdIndexedDB(obj);
+    if(data !== null) SearchSuccessCbFunc(data, obj);
+  }
 }
