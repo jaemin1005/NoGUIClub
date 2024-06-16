7a16e55b31003146897a288b42e318cf11d9590d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-16 23:27:41 +0900

    New | SendData
    serverState.MySQLConnect의 상태에 따라 데이터 보내는 곳이 분기됨
    MySql서버가 On 상태라면, 서버의 MySQL에 데이터를 CRUD 한다.
    MySql서버가 Off 상태라면, 브라우저의 IndexedDB에 데이터를 CRUD 한다

---
+++ src/client/controllers/commands/SendData.ts
@@ -0,0 +1,13 @@
+import { POSTFetch } from "client/modules/POSTFetch";
+import { serverState } from "../GetServerState";
+import { TypeSendData as TypeSendData } from "client/interface/TypeSendData";
+import { ProcessIndexedDB } from "../IndexedDBController/ProcessIndexedDB";
+
+export const SendData : TypeSendData = (url, obj, cbFunc, errCbFunc) => {
+  if(serverState.MySQLConnect === false){
+    ProcessIndexedDB(url,obj, cbFunc, errCbFunc);
+  }
+  else{
+    POSTFetch(url, JSON.stringify(obj), cbFunc, errCbFunc);
+  }
+}
