0b0d4b2eb449f7d0d8c03aab785f6df0a2a1ea21
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-16 23:55:08 +0900

    Modify | TypeSendData
    TypeSendData의 매개변수 콜백함수에서 ? 옵셔널 체이닝 추가

--- src/client/interface/TypeSendData.ts
+++ src/client/interface/TypeSendData.ts
@@ -2,5 +2,5 @@ import { ICommandData } from "@shared/interface/ICommand";
 import { IData } from "@shared/interface/IData";
 
 export type ProcessData = IData | ICommandData;
-export type TypeSendData = (url : string, obj : ProcessData, cbFunc : IResponseCbFunc, errCbFunc : IResponseCbFunc) => void;
+export type TypeSendData = (url : string, obj : ProcessData, cbFunc? : IResponseCbFunc, errCbFunc? : IResponseCbFunc) => void;
 export type ProcessCmdIndexedDB = (obj : {}, cbFunc : Function, errCbFunc : Function ) => void;
