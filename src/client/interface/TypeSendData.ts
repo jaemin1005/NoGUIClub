670fda600cf3b2871720db0f84eda54f64eaef0d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-16 21:15:52 +0900

    Modify | TypeSendData
    매개변수 obj의 타입을 따로 타입으로 분리하였습니다.

--- src/client/interface/TypeSendData.ts
+++ src/client/interface/TypeSendData.ts
@@ -1,2 +1,6 @@
-type SendData = (url : string, obj : {}, cbFunc : IResponseCbFunc, errCbFunc : IResponseCbFunc) => void;
-type ProcessCmdIndexedDB = (obj : {}, cbFunc : Function, errCbFunc : Function ) => void;
+import { ICommandData } from "@shared/interface/ICommand";
+import { IData } from "@shared/interface/IData";
+
+export type ProcessData = IData | ICommandData;
+export type TypeSendData = (url : string, obj : ProcessData, cbFunc : IResponseCbFunc, errCbFunc : IResponseCbFunc) => void;
+export type ProcessCmdIndexedDB = (obj : {}, cbFunc : Function, errCbFunc : Function ) => void;
