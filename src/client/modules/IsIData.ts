03c3726bdfdbf6eb4b0ad455ccc4eac638ec0cdb
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-16 21:30:32 +0900

    new | IsIData
    ProcessData를 IData로 분리하기 위한 타입가드

---
+++ src/client/modules/IsIData.ts
@@ -0,0 +1,6 @@
+import { IData } from "@shared/interface/IData";
+import { ProcessData } from "client/interface/TypeSendData";
+
+export function IsIData(obj : ProcessData) : obj is IData {
+  return (obj as IData).head !== undefined && (obj as IData).date !== undefined && (obj as IData).body !== undefined
+}
