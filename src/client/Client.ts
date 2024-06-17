b3c3978a485b00a721d3e103c40655288efc8bd0
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-17 10:01:48 +0900

    Modfiy | Client
    필요없는 import와 주석부분을 제거하였다.
    focus를 주는 이벤트를 제거하였다.

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,15 +1,9 @@
-import { ForceFocus } from "./modules/Focus";
-import { AdjustWidth, keydownEvent} from "./modules/KeyboardEvent";
+import { AdjustWidth } from "./modules/KeyboardEvent";
 import { eventController } from "./controllers/EventController";
 import { NormalEvent } from "./modules/ArrNormalEvent";
-import { DOMObserver } from "./modules/DOMObserver";
 import { mainObserver } from "./controllers/MainViewObserver";
-import { serverState } from "./controllers/GetServerState";
 
-console.dir(serverState);
 const $input = document.getElementById("command-text")! as HTMLInputElement;
-document.addEventListener("DOMContentLoaded", () => {ForceFocus($input)});
-//document.addEventListener("DOMContentLoaded", () => {$input.focus()});
 $input.addEventListener("input", AdjustWidth());
 eventController.AddStash(NormalEvent());
 mainObserver.StartObserve();
