a99bcefffb21eefd2384c05f48159cb3abf2c40e
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-22 14:36:40 +0900

    Modify | CallIndexedDB
    경로 변경 clinet => @client

--- src/client/controllers/IndexedDBController/CallIndexedDB.ts
+++ src/client/controllers/IndexedDBController/CallIndexedDB.ts
@@ -1,6 +1,6 @@
-import { IndexedDB } from "client/modules/IndexedDB/IndexedDB";
-import { IndexedDBTable } from "client/modules/IndexedDB/IndexedDBTable";
-import { DbTableName, DbTableKey } from "client/modules/Enum/EnumDbTableName";
+import { IndexedDB } from "@client/modules/IndexedDB/IndexedDB";
+import { IndexedDBTable } from "@client/modules/IndexedDB/IndexedDBTable";
+import { DbTableName, DbTableKey } from "@client/modules/Enum/EnumDbTableName";
 
 const postTable = new IndexedDBTable(DbTableName.POST, DbTableKey.POST);
 const indexedDB = new IndexedDB("NoGUIClub", [postTable]) 
