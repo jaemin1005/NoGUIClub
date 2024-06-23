66a6dea3771c4833f4a70539182495738107c000
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-23 13:07:39 +0900

    Modify | EscQuitFunc
    필요없는 모듈 호출 제거 및, import 호출 경로 변경

--- src/client/controllers/commands/create/EscQuitFunc.ts
+++ src/client/controllers/commands/create/EscQuitFunc.ts
@@ -1,8 +1,4 @@
-import { DeleteView } from "../../../modules/DeleteView";
-import { OnDisplayView } from "../../../modules/OnDisplayView";
-import { eventController } from "../../EventController";
-import { NormalEvent } from "../../../modules/ArrNormalEvent";
-import { InitStateView } from "@client/controllers/InitStateView";
+import { InitStateView } from "@client/controllers/DOMController/InitStateView";
 
 /**
  * 
