e5390f70fb0430a1d5b6d686e5b0e55fa56a20cd
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-23 13:24:00 +0900

    Modify | CreateCmdIndexedDB
    파일 이동에 따른 import 경로 변경

--- src/client/controllers/IndexedDBController/CreateCmdIndexedDB.ts
+++ src/client/controllers/IndexedDBController/CreateCmdIndexedDB.ts
@@ -2,7 +2,7 @@ import { indexedDB } from "./CallIndexedDB";
 import { DbTableName } from "@client/modules/Enum/EnumDbTableName";
 import { IData } from "@shared/interface/IData";
 import { SaveFailFunc, SaveSuccessFunc } from "../commands/create/EscSaveCbFunc";
-import { mapDOM } from "@client/modules/GetDOM";
+import { mapDOM } from "@client/modules/DOM/GetDOM";
 
 export const CreateCmdIndexedDB = async (obj : IData, cbFunc? : Function, errCbFunc? : Function ) => {
   const result = await indexedDB.Add(DbTableName.POST, obj);
