b0d72c5dce6459d23688b9f70651a1d35a67b12b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-23 13:13:07 +0900

    Modify | ErrorCommand
    파일의 이동에 따른 import 호출 경로 변경

--- src/client/controllers/commands/ErrorCommand.ts
+++ src/client/controllers/commands/ErrorCommand.ts
@@ -1,5 +1,5 @@
-import { AddChildInRootElement } from "../../modules/AddChildInRootElement";
-import { CreateElement } from "../../modules/CreateElement";
+import { AddChildInRootElement } from "../DOMController/AddChildInRootElement";
+import { CreateElement } from "../../modules/DOM/CreateElement";
 
 export function AddErrorInView(errMsg : string, arrErrSubMsg : string[]){
   
