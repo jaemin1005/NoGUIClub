03ec8ad53c37cb432119463ecf476e9479de6dcf
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-04 23:52:11 +0900

    Feat | Client.ts : 번들링작업을 위한 scss 호출

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -2,6 +2,7 @@ import { AdjustWidth } from "./controllers/Event/KeyboardEvent";
 import { eventController } from "./modules/DOM/EventController";
 import { NormalEvent } from "./controllers/Event/ArrNormalEvent";
 import { mainObserver } from "./controllers/DOMController/MainViewObserver";
+import '../../public/scss/main.scss';
 
 const $input = document.getElementById("command-text")! as HTMLInputElement;
 $input.addEventListener("input", AdjustWidth());
