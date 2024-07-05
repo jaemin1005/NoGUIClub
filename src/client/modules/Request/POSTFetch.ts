925be34adb520170604c445e727c990f26e889aa
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-05 17:10:43 +0900

    Feat | PostFetch : 서버와의 연결이 실패했을 떄, 사용자에게 알려준다

--- src/client/modules/Request/POSTFetch.ts
+++ src/client/modules/Request/POSTFetch.ts
@@ -1,13 +1,19 @@
-export async function POSTFetch(host: string, url : string, data : string, callback? : IResponseCbFunc, errCallback? : IResponseCbFunc){
-  let res = await fetch(host+url,{ 
-    method : "POST",
-    headers : {
-      'Content-Type': 'application/json'
-    },
-    body : data });
-  if(res.status === 200){
-    if(callback) callback(res);
-  }else{
-    if(errCallback) errCallback(res);
+export async function POSTFetch(host: string, url: string, data: string, callback?: IResponseCbFunc, errCallback?: IResponseCbFunc) {
+  try {
+    let res = await fetch(host + url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    });
+    if (res.status === 200) {
+      if (callback) callback(res);
+    } else {
+      if (errCallback) errCallback(res);
+    }
+  }
+  catch (err) {
+    alert('서버와의 연결에 실패했습니다. 로컬로 기록됩니다.');
   }
 }
