be978cad66c584adac386ff74d7ff0dfb4f1ea12
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-05 17:48:19 +0900

    Fix | ExecSearchCbFunc.ts : 데이터 길이가 없을 떄, 그대로 return
    콜백함수가 필요할지도

--- src/client/controllers/commands/search/ExecSearchCbFunc.ts
+++ src/client/controllers/commands/search/ExecSearchCbFunc.ts
@@ -9,6 +9,9 @@ import { SearchListView } from "./SearchListView";
 
 /** Search 명령어가 성공했을 떄 콜백되는 메소드 */
 export async function SearchSuccessCbFunc(data : IData[], commandData : ICommandData){
+
+  if(data.length === 0) return;
+
   const elem = mapDOM.GetDOM("command-text")! as HTMLInputElement;
   ClearView(mapDOM.GetDOM("main-view")!);
   const keyboardEvent = new SearchKeyboardEvent(elem, commandData);
